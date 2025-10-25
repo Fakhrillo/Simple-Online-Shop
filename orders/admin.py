935f558b490691e19b9ea7b4b5a17c66c72bad50
Fakhrillo <dombit1999@gmail.com>
2025-10-25 20:50:29 +0500

    pdf printer added

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,7 @@ from orders.models import Order, OrderItem
 from django_daisy.mixins import NavTabMixin
 from django.utils.safestring import mark_safe
 from orders.custom_actions import export_to_csv
+from django.urls import reverse
 # Register your models here.
 
 def order_payment(obj):
@@ -14,13 +15,26 @@ def order_payment(obj):
 
 order_payment.short_description = 'Stripe payment'
 
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
+
 class OrderItemInline(admin.TabularInline, NavTabMixin):
     model = OrderItem
     raw_id_fields = ['product']
 
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated', order_detail, order_pdf]
     list_display_links = ['id', 'first_name', 'last_name']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from orders import views
+from orders.custom_view import admin_order_detail, admin_order_pdf
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create/', views.order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', admin_order_detail, name="admin_order_detail"),
+    path('admin/order/<int:order_id>/pdf/',admin_order_pdf, name='admin_order_pdf'),
 ]
