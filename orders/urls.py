6f02bf11b7963b1a1e36b970fa1b36e319468664
Fakhrillo <dombit1999@gmail.com>
2025-12-16 13:39:46 +0500

    added url translations

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from orders import views
 from orders.custom_view import admin_order_detail, admin_order_pdf
+from django.utils.translation import gettext_lazy as _
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('create/', views.order_create, name='order_create'),
+    path(_('create/'), views.order_create, name='order_create'),
     path('admin/order/<int:order_id>/', admin_order_detail, name="admin_order_detail"),
     path('admin/order/<int:order_id>/pdf/',admin_order_pdf, name='admin_order_pdf'),
 ]
